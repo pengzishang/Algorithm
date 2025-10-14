0ccfe198185725701868f63debda3af1220d96a6
Eric Peng <zpeng0022@conestogac.on.ca>
2025-10-13 22:58:56 -0400

    feat: ListNode function

--- Sources/LeetCodeSolutions/Utils/ListNode.swift
+++ Sources/LeetCodeSolutions/Utils/ListNode.swift
@@ -1,5 +1,37 @@
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init(_ val: Int) { self.val = val; self.next = nil }
+public class ListNode<T> {
+    public var val: T
+    public var next: ListNode<T>?
+    public init(_ val: T) {
+        self.val = val
+        self.next = nil
+    }
+    public init(_ val: T, _ next: ListNode<T>?) {
+        self.val = val
+        self.next = next
+    }
+}
+
+public extension Array {
+    func toLinkedList() -> ListNode<Element>? {
+        guard !isEmpty else { return nil }
+        let dummy = ListNode(self[0])
+        var current = dummy
+        for val in self.dropFirst() {
+            current.next = ListNode(val)
+            current = current.next!
+        }
+        return dummy
+    }
+}
+
+public extension ListNode where T: CustomStringConvertible {
+    func printList() {
+        var node: ListNode? = self
+        var result: [String] = []
+        while let current = node {
+            result.append(String(describing: current.val))
+            node = current.next
+        }
+        print(result.joined(separator: " -> ") + " -> nil")
+    }
 }
