331dce242ceda57fc2ac5c1f087f37213fbec2b4
Zishang Peng <pengzishang@hotmail.com>
2025-10-29 17:37:42 -0400

    2469: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P2469_Convert the Temperature/P2469_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P2469_Convert the Temperature/P2469_Solution1.swift
@@ -1,5 +1,7 @@
 final class P2469_Solution1 {
     func convertTemperature(_ celsius: Double) -> [Double] {
-        
+        let kelvin = celsius + 273.15
+        let fahrenheit = celsius * 1.80 + 32.00
+        return [kelvin, fahrenheit]
     }
 }
