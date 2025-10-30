a290a810a1301bd6c9b978ee512ee4e49b8f0204
Zishang Peng <pengzishang@hotmail.com>
2025-10-30 12:41:55 -0400

    1512: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P1512_Number of Good Pairs/P1512_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P1512_Number of Good Pairs/P1512_Solution1.swift
@@ -1,5 +1,15 @@
 final class P1512_Solution1 {
     func numIdenticalPairs(_ nums: [Int]) -> Int {
-        
+        nums
+            .reduce(into: [Int: Int]()) {
+                if let value = $0[$1] {
+                    $0[$1] = value + 1
+                } else {
+                    $0[$1] = 1
+                }
+            }
+            .reduce(into: Int()) {
+                $0 += ($1.value * ($1.value - 1) / 2)
+            }
     }
 }
