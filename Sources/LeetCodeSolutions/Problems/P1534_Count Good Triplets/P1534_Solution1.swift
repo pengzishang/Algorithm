3251c3e2b52af26956e84c71f48e85f5ccad5e02
Zishang Peng <pengzishang@hotmail.com>
2025-10-30 21:40:10 -0400

    1534: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P1534_Count Good Triplets/P1534_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P1534_Count Good Triplets/P1534_Solution1.swift
@@ -1,5 +1,15 @@
 final class P1534_Solution1 {
     func countGoodTriplets(_ arr: [Int], _ a: Int, _ b: Int, _ c: Int) -> Int {
-        
+        var count = 0
+        for i in 0 ..< arr.count - 2 {
+            for j in i + 1 ..< arr.count - 1 {
+                for k in j + 1 ..< arr.count {
+                    if abs(arr[i] - arr[j]) <= a, abs(arr[j] - arr[k]) <= b, abs(arr[i] - arr[k]) <= c {
+                        count += 1
+                    }
+                }
+            }
+        }
+        return count
     }
 }
