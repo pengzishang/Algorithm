eb325607be1b8e3ded48b067d29187673dec2425
Zishang Peng <pengzishang@hotmail.com>
2025-11-01 18:02:14 -0400

    1281: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P1281_Subtract the Product and Sum of Digits of an Integer/P1281_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P1281_Subtract the Product and Sum of Digits of an Integer/P1281_Solution1.swift
@@ -1,5 +1,14 @@
 final class P1281_Solution1 {
     func subtractProductAndSum(_ n: Int) -> Int {
-        
+        var copyN = n
+        var sum = 0
+        var mul = 1
+        while copyN != 0 {
+            let lastDig = copyN % 10
+            sum += lastDig
+            mul *= lastDig
+            copyN /= 10
+        }
+        return mul - sum
     }
 }
