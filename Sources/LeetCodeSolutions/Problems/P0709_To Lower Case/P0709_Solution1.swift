79b218bfde7be98ce5f17c368e38a21b38aacce5
Zishang Peng <pengzishang@hotmail.com>
2025-10-31 21:21:16 -0400

    0709: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P0709_To Lower Case/P0709_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P0709_To Lower Case/P0709_Solution1.swift
@@ -1,5 +1,11 @@
 final class P0709_Solution1 {
     func toLowerCase(_ s: String) -> String {
-        
+        let array = s.data(using: .utf8)?.compactMap {
+            if $0 >= 65, $0 <= 90 {
+                return $0 | (1 << 5)
+            }
+            return $0
+        }
+        return String(bytes: array!, encoding: .utf8)!
     }
 }
