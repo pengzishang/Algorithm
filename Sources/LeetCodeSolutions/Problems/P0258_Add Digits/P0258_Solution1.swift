b0f38307ea7657ec4ab3803e12c17a30142e87ac
Zishang Peng <pengzishang@hotmail.com>
2025-11-01 13:08:10 -0400

    0258: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P0258_Add Digits/P0258_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P0258_Add Digits/P0258_Solution1.swift
@@ -1,5 +1,15 @@
 final class P0258_Solution1 {
     func addDigits(_ num: Int) -> Int {
-        
+        if num < 10 {
+            return num
+        }
+        let row = firstRound(num: num)
+        func firstRound(num: Int) -> Int {
+            if num == 0 {
+                return 0
+            }
+            return num % 10 + firstRound(num: num / 10)
+        }
+        return addDigits(row)
     }
 }
