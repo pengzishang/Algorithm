f1491338001ad9a74f379aba0f390cdf5d385bcf
Zishang Peng <pengzishang@hotmail.com>
2025-11-04 21:46:43 -0500

    1470: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P1470_Shuffle the Array/P1470_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P1470_Shuffle the Array/P1470_Solution1.swift
@@ -1,5 +1,9 @@
 final class P1470_Solution1 {
     func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
-        
+        var result = [Int]()
+        for i in 0 ..< n {
+            result.append(contentsOf: [nums[i], nums[n + i]])
+        }
+        return result
     }
 }
