4cb1d8834ac7c0b04f6cff48c6c2fc40201aeab1
Zishang Peng <pengzishang@hotmail.com>
2025-11-07 15:55:15 -0500

    0643: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P0643_Maximum Average Subarray I/P0643_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P0643_Maximum Average Subarray I/P0643_Solution1.swift
@@ -1,5 +1,14 @@
 final class P0643_Solution1 {
     func findMaxAverage(_ nums: [Int], _ k: Int) -> Double {
-        
+        var currSum = nums.prefix(k).reduce(into: Int()) { $0 += $1 }
+        var maxAvg = Double(currSum) / Double(k)
+        if nums.count > k {
+            for i in 1 ... nums.count - k {
+                currSum -= nums[i - 1]
+                currSum += nums[i + k - 1]
+                maxAvg = max(maxAvg, Double(currSum) / Double(k))
+            }
+        }
+        return maxAvg
     }
 }
