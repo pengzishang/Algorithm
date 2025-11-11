6becdb2f59adf95e489a0ecbc0374d0ca6b0887c
Zishang Peng <pengzishang@hotmail.com>
2025-11-10 21:54:29 -0500

    feat: interview test
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/Sandbox/main.swift
+++ Sources/Sandbox/main.swift
@@ -1,20 +1,112 @@
 import Foundation
 import LeetCodeSolutions
 
-// var nums1 = [1,2,3,0,0,0]
-// P0088_Solution1().merge(&nums1, 3, [2,5,6], 3)
-// print("P0088:", nums1)
+// start from here.
+// Step 1
+func fetchHTML(_ urlString: String) async throws -> String {
+    guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+    let (data, _) = try await URLSession.shared.data(from: url)
+    guard let html = String(data: data, encoding: .utf8) else { throw URLError(.cannotDecodeRawData) }
+    return html
+}
 
-// let res = P0001_Solution1().twoSum([2,7,11,15], 9)
-// print("P0001:", res)
+private func htmlUnescape(_ s: String) -> String {
+    var t = s
+    let ents = ["&nbsp;": " ",
+                "&lt;": "<",
+                "&gt;": ">",
+                "&amp;": "&",
+                "&quot;": "\"",
+                "&#39;": "'"]
+    for (k, v) in ents {
+        t = t.replacingOccurrences(of: k, with: v)
+    }
+    return t.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
+}
 
-// var num = [1,2,3]
-// //let res = P0283_Solution3().moveZeroes(&num)
-// let head = [5,2,13,3,8].toLinkedList()
-// head?.printList()
-// let newHead = P2487_Solution2().removeNodes(head)
-// newHead?.printList()
+// Step 2
+private func parsePoints(_ html: String) -> [(x: Int, y: Int, ch: Character)] {
+    guard let table = try? NSRegularExpression(pattern: "<table[\\s\\S]*?</table>", options: .caseInsensitive) else {
+        return []
+    }
+    guard let m = table.firstMatch(in: html, range: NSRange(html.startIndex..., in: html)),
+          let range = Range(m.range, in: html)
+    else {
+        return []
+    }
+    let tab = String(html[range])
+    guard let tr = try? NSRegularExpression(pattern: "<tr[\\s\\S]*?</tr>", options: .caseInsensitive) else {
+        return []
+    }
+    let trs = tr.matches(in: tab, range: NSRange(tab.startIndex..., in: tab))
+    guard let td = try? NSRegularExpression(pattern: "<t[dh][^>]*>([\\s\\S]*?)</t[dh]>", options: .caseInsensitive)
+    else {
+        return []
+    }
+    var pts: [(Int, Int, Character)] = []
+    for (i, tm) in trs.enumerated() where i > 0 {
+        guard let r = Range(tm.range, in: tab) else {
+            continue
+        }
+        let rowStr = String(tab[r])
+        let rowNS = rowStr as NSString
+        let matches = td.matches(in: rowStr, range: NSRange(location: 0, length: rowNS.length))
+        let cells: [String] = matches.compactMap { m in
+            let capRange = m.range(at: 1)
+            guard capRange.location != NSNotFound,
+                  let swiftRange = Range(capRange, in: rowStr)
+            else {
+                return nil
+            }
+            let raw = String(rowStr[swiftRange])
+            return htmlUnescape(raw.trimmingCharacters(in: .whitespacesAndNewlines))
+        }
+        guard cells.count >= 3,
+              let x = Int(cells[0]),
+              let y = Int(cells[2])
+        else {
+            continue
+        }
+        pts.append((x, y, cells[1].first ?? " "))
+    }
+    return pts
+}
 
-// let nums1 = [4, 1, 2], nums2 = [1, 3, 4, 2]
-// let rArray = P2996_Solution1().missingInteger([1, 2, 3, 2, 5])
-// [-1,3,-1]
+// step 3
+private func printGrid(_ pts: [(x: Int, y: Int, ch: Character)]) {
+    guard !pts.isEmpty else {
+        print("No points")
+        return
+    }
+    let maxX = pts.map(\.x).max() ?? 0
+    let maxY = pts.map(\.y).max() ?? 0
+    var grid = Array(repeating: Array(repeating: Character(" "), count: maxX + 1), count: maxY + 1)
+    for p in pts {
+        if p.y >= 0, p.x >= 0, p.y < grid.count, p.x < grid[0].count {
+            grid[p.y][p.x] = p.ch
+        }
+    }
+    for row in grid {
+        print(String(row))
+    }
+}
+
+private func run() async {
+    let url = "https://docs.google.com/document/d/e/2PACX-1vRPzbNQcx5UriHSbZ-9vmsTow_R6RRe7eyAU60xIF9Dlz-vaHiHNO2TKgDi7jy4ZpTpNqM7EvEcfr_p/pub"
+    do {
+        let html = try await fetchHTML(url)
+        let pts = parsePoints(html)
+        printGrid(pts)
+    } catch {
+        print(error)
+    }
+}
+
+import Dispatch
+
+Task {
+    await run()
+    exit(0)
+}
+
+dispatchMain()
