330a40ba0e21a8429463377be7b5056b1f6bf4c1
Zishang Peng <pengzishang@hotmail.com>
2025-11-13 11:53:53 -0500

    2461: solution
    Signed-off-by: Zishang Peng <pengzishang@hotmail.com>

--- Sources/LeetCodeSolutions/Problems/P2461_Maximum Sum of Distinct Subarrays With Length K/P2461_Solution1.swift
+++ Sources/LeetCodeSolutions/Problems/P2461_Maximum Sum of Distinct Subarrays With Length K/P2461_Solution1.swift
@@ -1,5 +1,44 @@
 final class P2461_Solution1 {
     func maximumSubarraySum(_ nums: [Int], _ k: Int) -> Int {
-        
+        var currentArray: [Int] = Array(nums.prefix(k))
+        var currSum = currentArray.reduce(0, +)
+        var infos: [Int: Int] = currentArray.reduce(into: [Int: Int]()) { partialResult, item in
+            partialResult[item] = (partialResult[item] ?? 0) + 1
+        }
+        var maxSum = 0
+        if isSubArray(currentArray) {
+            maxSum = currSum
+        }
+
+        for i in k ..< nums.count {
+            if nums[i] == nums[i - k] {
+                continue
+            }
+            currSum += nums[i]
+            currSum -= nums[i - k]
+
+            currentArray += [nums[i]]
+            currentArray.removeFirst()
+
+            infos[nums[i]] = (infos[nums[i]] ?? 0) + 1
+
+            if let value = infos[nums[i - k]] {
+                if value > 1 {
+                    infos[nums[i - k]] = value - 1
+                } else {
+                    infos.removeValue(forKey: nums[i - k])
+                }
+            }
+
+            if isSubArray(currentArray) {
+                maxSum = max(maxSum, currSum)
+            }
+        }
+
+        func isSubArray(_ array: [Int]) -> Bool {
+            infos.count == array.count
+        }
+
+        return maxSum
     }
 }
